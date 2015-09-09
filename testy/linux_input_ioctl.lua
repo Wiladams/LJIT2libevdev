64bcab8de16e01cd28bdaead3359a106da9a525b
William A Adams <spam@nanotechstyles.com>
2015-09-09 08:53:59 -0700

    Messing with ioctl

--- testy/linux_input_ioctl.lua
+++ testy/linux_input_ioctl.lua
@@ -1,4 +1,131 @@
+--[[ 
+	Borrowed from ljsyscall
+--]]
+local bit = require "bit"
 
+local band = bit.band
+local function bor(...)
+  local r = bit.bor(...)
+  if r < 0 then r = r + 4294967296 end -- TODO see note in NetBSD
+  return r
+end
+local lshift = bit.lshift
+local rshift = bit.rshift
+
+-- these can vary by architecture
+local IOC = arch.IOC or {
+  SIZEBITS = 14,
+  DIRBITS = 2,
+  NONE = 0,
+  WRITE = 1,
+  READ = 2,
+}
+
+IOC.READWRITE = IOC.READ + IOC.WRITE
+
+IOC.NRBITS	= 8
+IOC.TYPEBITS	= 8
+
+IOC.NRMASK	= lshift(1, IOC.NRBITS) - 1
+IOC.TYPEMASK	= lshift(1, IOC.TYPEBITS) - 1
+IOC.SIZEMASK	= lshift(1, IOC.SIZEBITS) - 1
+IOC.DIRMASK	= lshift(1, IOC.DIRBITS) - 1
+
+IOC.NRSHIFT   = 0
+IOC.TYPESHIFT = IOC.NRSHIFT + IOC.NRBITS
+IOC.SIZESHIFT = IOC.TYPESHIFT + IOC.TYPEBITS
+IOC.DIRSHIFT  = IOC.SIZESHIFT + IOC.SIZEBITS
+
+local function ioc(dir, ch, nr, size)
+  if type(ch) == "string" then ch = ch:byte() end
+  return bor(lshift(dir, IOC.DIRSHIFT), 
+	     lshift(ch, IOC.TYPESHIFT), 
+	     lshift(nr, IOC.NRSHIFT), 
+	     lshift(size, IOC.SIZESHIFT))
+end
+
+local singletonmap = {
+  int = "int1",
+  char = "char1",
+  uint = "uint1",
+  uint32 = "uint32_1",
+  uint64 = "uint64_1",
+}
+local sizes = {
+	int1 = ffi.sizeof("int");
+	char1 = ffi.sizeof("char");
+	uint1 = ffi.sizeof("unsigned int");
+	uint32 = ffi.sizeof("uint32_t");
+	uint64 = ffi.sizeof("uint54_t");
+}
+
+local function _IOC(dir, ch, nr, tp)
+  if not tp or type(tp) == "number" then return ioc(dir, ch, nr, tp or 0) end
+  local size = sizes[tp]
+  local singleton = singletonmap[tp] ~= nil
+  tp = singletonmap[tp] or tp
+  return {number = ioc(dir, ch, nr, size),
+          read = dir == IOC.READ or dir == IOC.READWRITE, write = dir == IOC.WRITE or dir == IOC.READWRITE,
+          type = t[tp], singleton = singleton}
+end
+
+-- used to create numbers
+local _IO    = function(ch, nr)		return _IOC(IOC.NONE, ch, nr, 0) end
+local _IOR   = function(ch, nr, tp)	return _IOC(IOC.READ, ch, nr, tp) end
+local _IOW   = function(ch, nr, tp)	return _IOC(IOC.WRITE, ch, nr, tp) end
+local _IOWR  = function(ch, nr, tp)	return _IOC(IOC.READWRITE, ch, nr, tp) end
+
+-- used to decode ioctl numbers..
+local _IOC_DIR  = function(nr) return band(rshift(nr, IOC.DIRSHIFT), IOC.DIRMASK) end
+local _IOC_TYPE = function(nr) return band(rshift(nr, IOC.TYPESHIFT), IOC.TYPEMASK) end
+local _IOC_NR   = function(nr) return band(rshift(nr, IOC.NRSHIFT), IOC.NRMASK) end
+local _IOC_SIZE = function(nr) return band(rshift(nr, IOC.SIZESHIFT), IOC.SIZEMASK) end
+
+-- ...and for the drivers/sound files...
+
+IOC.IN		= lshift(IOC.WRITE, IOC.DIRSHIFT)
+IOC.OUT		= lshift(IOC.READ, IOC.DIRSHIFT)
+IOC.INOUT		= lshift(bor(IOC.WRITE, IOC.READ), IOC.DIRSHIFT)
+local IOCSIZE_MASK	= lshift(IOC.SIZEMASK, IOC.SIZESHIFT)
+local IOCSIZE_SHIFT	= IOC.SIZESHIFT
+
+
+
+-- event system
+  EVIOCGVERSION   = _IOR('E', 0x01, "int");		-- get driver version
+  EVIOCGID        = _IOR('E', 0x02, "input_id");	-- get device ID
+  EVIOCGREP       = _IOR('E', 0x03, "uint2");		-- get repeat settings
+  EVIOCSREP       = _IOW('E', 0x03, "uint2");		-- set repeat settings
+
+  EVIOCGKEYCODE   = _IOR('E', 0x04, "uint2");	-- get keycode
+  EVIOCGKEYCODE_V2 = _IOR('E', 0x04, "input_keymap_entry");
+  EVIOCSKEYCODE   = _IOW('E', 0x04, "uint2");	-- set keycode
+  EVIOCSKEYCODE_V2 = _IOW('E', 0x04, "input_keymap_entry");
+  
+  EVIOCGNAME = function(len) return _IOC(IOC.READ, 'E', 0x06, len) end;		-- get device name
+  EVIOCGPHYS = function(len) return _IOC(IOC.READ, 'E', 0x07, len) end;		-- get physical location
+  EVIOCGUNIQ = function(len) return _IOC(IOC.READ, 'E', 0x08, len) end;		-- get unique identifier
+  EVIOCGPROP = function(len) return _IOC(IOC.READ, 'E', 0x09, len) end;		-- get device properties
+  EVIOCGKEY  = function(len) return _IOC(IOC.READ, 'E', 0x18, len) end;
+  EVIOCGLED  = function(len) return _IOC(IOC.READ, 'E', 0x19, len) end;
+  EVIOCGSND  = function(len) return _IOC(IOC.READ, 'E', 0x1a, len) end;
+  EVIOCGSW   = function(len) return _IOC(IOC.READ, 'E', 0x1b, len) end;
+  EVIOCGBIT  = function(ev, len) return _IOC(IOC.READ, 'E', 0x20 + ev, len) end;
+  EVIOCGABS  = function(abs) return _IOR('E', 0x40 + abs, "input_absinfo") end;
+  EVIOCSABS  = function(abs) return _IOW('E', 0xc0 + abs, "input_absinfo") end;
+  EVIOCSFF   = _IOC(IOC.WRITE, 'E', 0x80, "ff_effect");
+  EVIOCRMFF  = _IOW('E', 0x81, "int");
+  EVIOCGEFFECTS = _IOR('E', 0x84, "int");
+  EVIOCGRAB  = _IOW('E', 0x90, "int");
+-- input devices
+  UI_DEV_CREATE  = _IO ('U', 1);
+  UI_DEV_DESTROY = _IO ('U', 2);
+  UI_SET_EVBIT   = _IOW('U', 100, "int");
+  UI_SET_KEYBIT  = _IOW('U', 101, "int");
+
+
+--[[
+Straight from the linux/input.h header
 
 #define EVIOCGVERSION		_IOR('E', 0x01, int)			/* get driver version */
 #define EVIOCGID			_IOR('E', 0x02, struct input_id)	/* get device ID */
@@ -36,4 +163,5 @@
 #define EVIOCREVOKE		_IOW('E', 0x91, int)			/* Revoke device access */
 
 #define EVIOCSCLOCKID		_IOW('E', 0xa0, int)			/* Set clockid to be used for timestamps */
+--]]
 
